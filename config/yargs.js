2d1a6a462c75d1f46ac943f9430e93e803d07e33
Rodrigo <rodrigo.csejas@gmail.com>
2019-03-19 17:07:14 -0400

    primer commit

---
+++ config/yargs.js
@@ -0,0 +1,42 @@
+const descripcion = {
+    descripcion: {
+        demand: true,
+        alias: 'd',
+        desc: 'Descripcion de la tarea por hacer'
+    }
+}
+
+
+const argv = require('yargs')
+    .command('crear','Crear un elemento por hacer', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de la tarea por hacer'
+        }
+    })
+    .command('actualizar','Actualiza el estado completado de una tarea',{
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de la tarea por hacer'
+        },
+        completado: {
+            default: true,
+            alias: 'c',
+            desc: 'Marca como completado o pendiente la tarea'
+        }
+    })
+    .command('borrar','Borrar un elemento', {
+        descripcion: {
+            demand: true,
+            alias: 'd',
+            desc: 'Descripcion de la tarea por Borrar'
+        }
+    })
+    .help()
+    .argv;
+
+    module.exports = {
+        argv
+    }
